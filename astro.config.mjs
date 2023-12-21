eed25dc52cb023787d26ca46afecd18e449df5e7
sonicj <sonicjhon56@gmail.com>
2023-12-21 15:35:32 +0700

    Fix broken links/assets if the website is in a subpath

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://localhost:4321",
   build: {
     // Generate `page.html` instead of `page/index.html` during build.
     format: "file"
