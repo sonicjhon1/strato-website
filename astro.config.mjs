a4ad1837eb555a11761745cdd9e7e3957a3479b2
sonicj <sonicjhon56@gmail.com>
2023-12-21 12:54:27 +0700

    Add optimisations and lazyload Team's pictures

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  build: {
+    // Generate `page.html` instead of `page/index.html` during build.
+    format: "file"
+  },
+  integrations: [tailwind(), compress()]
 });
